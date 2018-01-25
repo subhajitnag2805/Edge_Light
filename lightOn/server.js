cfc38fdce7f51639161d2d89d86b090cbb82d6b3
subhajitnag2805 <subhajit.nag@memeinfotech.com>
2018-01-25 04:50:22 +0000

    full cycle

--- lightOn/server.js
+++ lightOn/server.js
@@ -1,34 +1,59 @@
 var express = require('express');
 const SerialPort = require('serialport');
-
-const port = new SerialPort('/dev/ttyACM0',{
-	  baudRate: 9600
+var mongoose = require('mongoose');
+const serialport = new SerialPort('/dev/ttyACM0',{
+	baudRate: 9600
 });
 
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-io.on('connection', function(client){
-  console.log("User connected !");
-  
-  client.on('lightOnOff', function(data){
-	 let status = data.status;
-	 if(status == 'ON'){
-		    var buffer = new Buffer(1);
-            buffer.writeInt8(1);
-            port.write(buffer);
-		 } else if(status == 'OFF'){
-		    var buffer = new Buffer(1);
-            buffer.writeInt8(0);
-            port.write(buffer);	 
-	 } 
-  });
-  client.on('disconnect', function(){});
+var Test = require('./models/test');
+
+//connect to mongodb
+mongoose.connect('mongodb://test:password@ds211558.mlab.com:11558/ionic_chat');
+
+//on successful connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongodb!!');
 });
 
+//on error
+mongoose.connection.on('error', (err) => {
+    if (err) {
+        console.log('Error in db is :' + err);
+    }
+});
+
+io.on('connection', function(client){
+  console.log("Socket connected !");
+   client.on("generate",function(data){
+	   let status = data.status;
+	   if(status == "OK"){
+		   	var buffer = new Buffer(1);
+            buffer.writeInt8(5);
+            serialport.write(buffer);
+	   }
+   });
+   
+   	serialport.on('data', function(data){
+		console.log(data[0]);
+		let dataOne = new Test({
+			value: data[0]}).save(function(error,result){
+					if(error){
+						console.log(error);
+					}else if(result){
+						console.log(result);
+					}
+			});
+        client.emit('arduino', { "value":data[0] });
+	});
+   
+});
 
 const PORT = 7000;
 server.listen(PORT,function(){
-   console.log("Server running on port "+PORT);
+   console.log("Server started");	
 });
+
--- lightOnOff/src/app/app.module.ts
+++ lightOnOff/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { ChartsModule } from 'ng2-charts';
 import { AppComponent } from './app.component';
+import { GraphComponent } from './graph/graph.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
