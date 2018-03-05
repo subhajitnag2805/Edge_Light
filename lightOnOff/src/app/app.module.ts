7940527bb542d3ca1aec12a7e4ae9bcdbecdcfc7
Subhajit Nag <subhajit.nag@memeinfotech.com>
2018-03-05 17:24:03 +0530

    cc

--- lightOnOff/src/app/app.module.ts
+++ lightOnOff/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ChartsModule } from 'ng2-charts';
 import { AppComponent } from './app.component';
-import { GraphComponent } from './graph/graph.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GraphComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
