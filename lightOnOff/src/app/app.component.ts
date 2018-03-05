439fc4fde88856d2325e1d8c035d3bf6eafe24fe
Subhajit Nag <subhajit.nag@memeinfotech.com>
2018-03-05 17:01:56 +0530

    code

--- lightOnOff/src/app/app.component.ts
+++ lightOnOff/src/app/app.component.ts
@@ -1,82 +1,23 @@
 import { Component } from '@angular/core';
 import * as io from "socket.io-client";
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  socket:any = null;
-  status:any = 'off';
-  value:any;
-  
-  public lineChartData:Array<any>=[{
-	data:[], label:'Series B'
-  }];
-  public lineChartLabels:Array<any>=[];
-  public lineChartOptions:any={
-	responsive:true
-  };
-  
-  public lineChartColors: Array<any> = [
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    }
-  ];
-  
-  public lineChartLegend:boolean=true;
-  public lineChartType:String='line';
+  socket: any = null;
 
   constructor() {
-      this.socket = io('http://localhost:7000');
+    this.socket = io('http://localhost:7000');
   }
-  
-  lightOn(){
-     this.socket.emit('lightOnOff', {status:'ON'});
-     this.status = 'on';
+
+  temperature() {
+    this.socket.emit('start', { status: 'temperature' });
   }
-  
-  lightOff(){
-     this.socket.emit('lightOnOff', {status:'OFF'});
-     this.status = 'off';
+
+  gsr() {
+    this.socket.emit('start', { status: 'gsr' });
   }
-  
-  toggleLight(){
-       if(this.status == 'off'){
-             this.lightOn();
-       }else{
-             this.lightOff();
-       }
-  }
-  
-  /**send status from frontend */
-  generateNumber(){
-     let _base = this;
-     _base.socket.emit('generate', {status:'OK'});
-     _base.socket.on('arduino',function(data){
-          _base.value=data.value;
-          _base.push(data);
-     });
-  }
-  
-  /** Insert data into graph and show*/
-	push(data)
-	{
-		
-		let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-		
-		 _lineChartData[0] = { data: new Array(this.lineChartData[0].data.length + 1), label: this.lineChartData[0].label };
-		_lineChartData[0].data = this.lineChartData[0].data;
-		_lineChartData[0].data.push(data.value);
-		this.lineChartLabels.push(new Date().getTime());
-		this.lineChartData = _lineChartData;
-		}
 }
